b98590fb518217d05771c20ea87704c35ba486c1
urvashichore <46674790+urvashichore@users.noreply.github.com>
2019-04-09 21:04:18 +0530

    Add files via upload

---
+++ role-based-shop-management-assignment-gradle/WebContent/WEB-INF/pages/usermgmt.jsp
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>User Management Tab</title>
+</head>
+<body>
+<table border=2%>
+<tr>
+<th>User</th>
+<th>Role</th>
+</tr>
+<tr>
+<td>billing user</td>
+<td>BILLING USER</td>
+</tr>
+<tr>
+<td>user</td>
+<td>USER</td>
+</tr>
+<tr>
+<td>adminr</td>
+<td>ADMIN</td>
+</tr>
+</table>
+</body>
+</html>
---
+++ role-based-shop-management-assignment-gradle/src/main/java/smgmt/handler/AppSuccessHandler.java
@@ -0,0 +1,86 @@
+package smgmt.handler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+public class AppSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException {
+        String targetUrl = determineTargetUrl(authentication);
+
+        if (response.isCommitted()) {
+            System.out.println("Can't redirect");
+            return;
+        }
+
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(Authentication authentication) {
+        String url = "";
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        List<String> roles = new ArrayList<String>();
+
+        for (GrantedAuthority a : authorities) {
+            roles.add(a.getAuthority());
+        }
+
+        if (isAdmin(roles)) {
+            url = "/services/adminpage";
+        } else if (isUser(roles)) {
+            url = "/services/userpage";
+        } else if (isBillingUser(roles)) {
+        	url = "/services/billinguserpage";
+        } else {
+            url = "/services/accessDenied";
+        }
+
+        return url;
+    }
+
+    private boolean isUser(List<String> roles) {
+        if (roles.contains("ROLE_USER")) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isAdmin(List<String> roles) {
+        if (roles.contains("ROLE_ADMIN")) {
+            return true;
+        }
+        return false;
+    }
+    
+    private boolean isBillingUser(List<String> roles) {
+        if (roles.contains("ROLE_BILLINGUSER")) {
+            return true;
+        }
+        return false;
+    }
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
+    }
+
+    protected RedirectStrategy getRedirectStrategy() {
+        return redirectStrategy;
+    }
+
+}
